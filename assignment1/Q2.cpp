8a9297d4403b7b277a61cb65c794dfe5ab408193
Aarav Singhal <aaravsinghal2005@gmail.com>
2025-08-07 18:59:55 +0530

    finished duplicate array code

--- assignment1/Q2.cpp
+++ assignment1/Q2.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-    
-    int arr[] = {1,2,2,2,3,4,4,4,5,5,5,6,6,6,6,7,7,7};
-    
-    int l = sizeof(arr)/sizeof(arr[0]);
+int arr[] = {1,2,2,2,3,4,4,4,5,5,5,6,6,6,6,7,7,7};
+int l = sizeof(arr)/sizeof(arr[0]);
+void del(int index, int &len) {
+    for (int i = index; i < len - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    len--;
+}
+int main() {
     int dup_ind[l];
     int count = 0;
     int unique = 0;
     int k = 0;
-    for(int i  = 0;i<l;i++)
-    {
+    for (int i = 0; i < l; i++) {
         unique++;
         int numb = arr[i];
-        for(int j = i+1;j<l;j++)
-        {
-            if(arr[j]==numb)
-            {
+        for (int j = i + 1; j < l; j++) {
+            if (arr[j] == numb) {
                 count++;
-                dup_ind[k] = j;
-                k++;
+                dup_ind[k++] = j;
             }
         }
-        i = i+count;
+        i = i + count;
         count = 0;
     }
-    cout<<unique<<endl;
-    for(int i = 0;i<l-unique;i++)
-    {
-        cout<<dup_ind[i]<<",";//duplicate indexes
+    cout << "Original array: ";
+    for (int i = 0; i < l; i++) {
+        cout << arr[i] << ",";
     }
+    cout << endl;
+    //reverse delete -- implemented because when we do direct deletion once we delete an element the next element goes into the current spot but we needed to delete the element before it.
+    for (int i = l - unique - 1; i >= 0; i--) {
+        del(dup_ind[i], l);
+    }
+    cout << "Final array: ";
+    for (int i = 0; i < l; i++) {
+        cout << arr[i] << ",";
+    }
+    cout << endl;
     return 0;
 }
