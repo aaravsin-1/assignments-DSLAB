48047fd0a95a33a0621b2dcd77e1d4b633feab04
Aarav Singhal <aaravsinghal2005@gmail.com>
2025-08-21 09:47:14 +0530

    Update Q8.cpp

--- assignment2/Q8.cpp
+++ assignment2/Q8.cpp
@@ -17,7 +17,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
         bool isDuplicate = false;
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i; j++)//new elememnt counted only once
         {
             if (arr[i] == arr[j])
             {
