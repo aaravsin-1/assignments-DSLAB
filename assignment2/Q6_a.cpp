dfd1373ecab014e1ce487f2ce0639b21187d182f
Aarav Singhal <aaravsinghal2005@gmail.com>
2025-08-21 09:32:41 +0530

    updates q6_a

--- assignment2/Q6_a.cpp
+++ assignment2/Q6_a.cpp
@@ -2,66 +2,71 @@
 using namespace std;
 
 int n = 6;
+int m = 6;
 int sparse[6][6] = {
     {0, 0, 5, 0, 0, 0},
     {0, 0, 0, 0, 8, 0},
     {0, 0, 0, 0, 0, 0},
     {0, 0, 0, 6, 0, 0},
     {1, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 3, 0}
-};
-
-void transposeArray(int s, int arr[][3])
-{
-    int trans[3][100]; 
-    for (int i = 0; i < 3; i++) 
-    {
-        for (int j = 0; j < s; j++) 
-        {
-            trans[i][j] = arr[j][i];
-        }
-    }
-
-    cout << "\nFull Transposed Array:\n";
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < s; j++)
-        {
-            cout << trans[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+    {0, 0, 0, 0, 3, 0}};
 
 int main()
 {
     int size = 0;
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             if (sparse[i][j] != 0)
                 size++;
-
-    int trip[size][3];
+        }
+    }
+    int triplet[size][3];
     int k = 0;
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             if (sparse[i][j] != 0)
             {
-                trip[k][0] = i;
-                trip[k][1] = j;
-                trip[k][2] = sparse[i][j];
+                triplet[k][0] = i;
+                triplet[k][1] = j;
+                triplet[k][2] = sparse[i][j];
                 k++;
             }
-
-    cout << "Original Triplet Array:\n";
+        }
+    }
+    int transpose_triplet[size][3];
     for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < 3; j++)
+        transpose_triplet[i][0] = triplet[i][1];
+        transpose_triplet[i][1] = triplet[i][0];
+        transpose_triplet[i][2] = triplet[i][2];
+    }
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
         {
-            cout << trip[i][j] << " ";
+            if (transpose_triplet[j][0] > transpose_triplet[j + 1][0] ||
+                (transpose_triplet[j][0] == transpose_triplet[j + 1][0] && transpose_triplet[j][1] > transpose_triplet[j + 1][1]))
+            {
+                for (int t = 0; t < 3; t++)
+                {
+                    int temp = transpose_triplet[j][t];
+                    transpose_triplet[j][t] = transpose_triplet[j + 1][t];
+                    transpose_triplet[j + 1][t] = temp;
+                }
+            }
         }
-        cout << endl;
+    }
+    cout << "\nTransposed triplet (sorted):" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << transpose_triplet[i][0] << " "
+             << transpose_triplet[i][1] << " "
+             << transpose_triplet[i][2] << endl;
     }
 
-    transposeArray(size, trip);
+    return 0;
 }
