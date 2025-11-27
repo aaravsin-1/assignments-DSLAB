79e09eb57073cf63e550926e8248d1be5f035402
Aarav Singhal <aaravsinghal2005@gmail.com>
2025-11-27 11:43:53 +0530

    graph class using finsihed

--- assignment9/graph_main.cpp
+++ assignment9/graph_main.cpp
@@ -1,47 +1,107 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <stack>
 using namespace std;
 
 class Graph
 {
     struct Edge
     {
-        int val;
+        int node;
         float weight;
-        Edge(int v,int w):val(v),weight(w){}
+        Edge(int v,float w):node(v),weight(w){}
     };
+
     int V;
     vector<vector<Edge>> adj;
-    public:
-        Graph(int N = 0): V(N),adj(N){};
-        void addEdge(int u,int v,int w);
-        void printGraph();
-        void bfs();
-        void dfs();
-};
-void Graph::addEdge(int u,int v,int w)
-{
-    if(u>=V || v>=V)
+
+public:
+    Graph(int N = 0): V(N), adj(N) {}
+
+    void addEdge(int u,int v,float w)
     {
-        cout<<"index out of bounds"<<endl;
-        return;
-    }
-    adj[u].push_back(Edge(v,w));
-    adj[v].push_back(Edge(u,w));//undirected graph
-}
-void Graph::printGraph()
-{
-    for(int u = 0;u<V;u++)
-    {
-        cout<<u<<":";
-        for(auto &e:adj[u])
+        if(u>=V || v>=V)
         {
-            cout<<"("<<e.val<<","<<e.weight<<")"<<",";
+            cout<<"index out of bounds"<<endl;
+            return;
+        }
+        adj[u].push_back(Edge(v,w));
+        adj[v].push_back(Edge(u,w));
+    }
+
+    void printGraph()
+    {
+        for(int u=0;u<V;u++)
+        {
+            cout << u << ": ";
+            for(const auto &e: adj[u])
+            {
+                cout << "(" << e.node << "," << e.weight << ") ";
+            }
+            cout << endl;
+        }
+    }
+
+    void bfs(int start=0)
+    {
+        vector<bool> visited(V,false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+
+            cout<<u<<" ";
+
+            for(const auto &e : adj[u])
+            {
+                int v = e.node;
+                if(!visited[v])
+                {
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
         }
         cout<<endl;
     }
-}
-void Graph::bfs
+
+    void dfs(int start=0)
+    {
+        vector<bool> visited(V,false);
+        stack<int> s;
+
+        s.push(start);
+
+        while(!s.empty())
+        {
+            int u = s.top();
+            s.pop();
+
+            if (!visited[u])
+            {
+                visited[u] = true;
+                cout << u << " ";
+            }
+
+            for(auto it = adj[u].rbegin(); it != adj[u].rend(); ++it)
+            {
+                int v = it->node;
+                if(!visited[v])
+                {
+                    s.push(v);
+                }
+            }
+        }
+        cout << endl;
+    }
+};
+
 int main()
 {
     Graph myGraph(5);
@@ -50,5 +110,12 @@ int main()
     myGraph.addEdge(0,3,1.4);
     myGraph.addEdge(1,2,2.4);
     myGraph.addEdge(1,3,2.5);
+
     myGraph.printGraph();
+
+    cout << "BFS: ";
+    myGraph.bfs(0);
+
+    cout << "DFS: ";
+    myGraph.dfs(0);
 }
